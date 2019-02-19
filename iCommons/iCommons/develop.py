fdbbc4cdcac0db5323232bc98e5f2799ef8f77dd
David Saks <das446@drexel.edu>
2019-02-19 16:55:09 -0500

    changed database settings

--- iCommons/iCommons/develop.py
+++ iCommons/iCommons/develop.py
@@ -3,5 +3,11 @@ from .settings import *
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__)))))
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/var/webapps/db/db.sqlite3',
+    }
+}
 DATABASE_NAME = '/var/webapps/db/db.sqlite3'
 ALLOWED_HOSTS = ['rack113.cs.drexel.edu', '129.25.13.213']
