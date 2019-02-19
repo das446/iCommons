08931760ff04d37247712a51460369d3abf2f613
David Saks <das446@drexel.edu>
2019-02-19 16:03:36 -0500

    changed database name

--- iCommons/iCommons/develop.py
+++ iCommons/iCommons/develop.py
@@ -2,6 +2,6 @@ from .settings import *
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__)))))
-print(ROOT_DIR)
 
+DATABASE_NAME = '/var/webapps/db/db.sqlite3'
 ALLOWED_HOSTS = ['rack113.cs.drexel.edu', '129.25.13.213']
