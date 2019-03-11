d5c7b36d16ffa50573fd5e23120b02698cde3cca
David Saks <das446@drexel.edu>
2019-03-10 23:22:31 -0400

    implemented available rooms

--- iCommons/Devices/views.py
+++ iCommons/Devices/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Device, DeviceType, LoanRequest, DeviceStatus
 import datetime
 import loanshark
+from iCommons import settings
+from django.core.mail import send_mail, EmailMultiAlternatives
 
 # Create your views here.
 
@@ -23,10 +25,28 @@ def reserve(request):
 
         device = loanshark.GetRandomDeviceOfType(device_type)
 
+        user = request.POST['user']
+
         # LoanRequest.objects.create(
         #     requester=request.user, device=device, start_time=datetime.datetime.now(), hours=time)
 
         context = {
             "Message": "You may pick up laptop "+device +" from Rush.",
         }
+
+        #Send confirmation email to user
+
+        send_email(user,"Device Reservation Confirmation",context["Message"])
+        notifySAs()
+
         return render(request, 'confirm.html', context)
+
+
+def send_email(user,subject,text):
+    from_email = settings.ihelp_email
+    to_email = user
+    print("from:" + from_email)
+    send_mail(subject, text,from_email, [to_email], fail_silently=False) 
+
+def notifySAs():
+    print("")
