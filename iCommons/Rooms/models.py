0aa64d88f512d1837469a7638105572662028e6e
David Saks <das446@drexel.edu>
2019-04-08 15:13:34 -0400

    began work on updating room functionality

--- iCommons/Rooms/models.py
+++ iCommons/Rooms/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 from Users.models import User
+import datetime
 
 # Create your models here.
 
@@ -19,19 +20,30 @@ class Room(models.Model):
     reservations = models.ManyToManyField("Reservation")
 
     def CurReservation(self,time):
-        for reservation in self.reservations:
-            if is_time_between(reservation.start_time, reservation.end_time, time):
+        for reservation in self.reservations.all():
+            if is_time_between(reservation.start_time.time, reservation.end_time.time, time):
                 return reservation
         return None
 
     def Available(self, time):
         return self.CurReservation(time) == None
 
-    def CurClass(self, time):
+    def GetClass(self, time):
         r = self.CurReservation(time)
         if r==None:
             return None
         return r.cur_class
+
+    @property
+    def CurClass(self):
+        return self.GetClass(datetime.datetime.now().time)
+
+    @property
+    def TestClass(self):
+        return self.reservations.all()[0].cur_class
+
+    def __str__(self):
+        return self.building + " - " + self.number
         
 
 class Class(models.Model):
@@ -39,26 +51,36 @@ class Class(models.Model):
     course_id = models.CharField(max_length=10) #CS 265
     teacher = models.ForeignKey(User)
 
+    def __str__(self):
+        return self.course_id
+
 
 class Reservation(models.Model):
     start_time = models.DateTimeField(null=True, blank=True)
     end_time = models.DateTimeField(null=True, blank=True)
     cur_class = models.ForeignKey(Class, null=True,blank=True)
     repeat = models.BooleanField(default = False)
+    repeat_weeks = models.IntegerField(default=1)
     description = models.CharField(max_length=1000, blank=True, null=True)
 
     def Description(self):
         if self.description != None and self.description != "":
             return self.description
         elif self.cur_class != None:
-            teacher = self.cur_class.teacher.FullName()
+            teacher = self.cur_class.teacher.full_name
             return "Occupied by "+teacher+"\nClass:"+self.cur_class.course_id 
         else:
             return ""
+    
+    def save(self, *args, **kwargs):
+        super(Reservation,self).save(*args, **kwargs)
+        #if self.repeat:
 
-def is_time_between(begin_time, end_time, check_time=None):
-# If check time is not given, default to current UTC time
-    check_time = check_time or datetime.utcnow().time()
+    def __str__(self):
+        return self.Description() +" "+ str(self.start_time) + " to " + str(self.end_time)
+
+
+def is_time_between(begin_time, end_time, check_time):
     if begin_time < end_time:
         return check_time >= begin_time and check_time <= end_time
     else: # crosses midnight
--- iCommons/iCommons/views.py
+++ iCommons/iCommons/views.py
@@ -2,9 +2,35 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from Rooms.models import Room
+import datetime
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')
+    occupied_rooms = GetOccupiedRooms()
+    available_rooms = GetAvailableRooms()
+    context = {
+        "available_rooms":available_rooms,
+        "occupied_rooms":occupied_rooms
+    }
+    return render(request, 'home.html', context)
+
+def GetOccupiedRooms():
+    occupied_rooms = []
+    rooms = Room.objects.all()
+    current_time = datetime.datetime.now
+    for room in rooms:
+        #if room.GetClass(current_time) is not None:
+        occupied_rooms.append(room)
+    return occupied_rooms
+
+def GetAvailableRooms():
+    available_rooms = []
+    rooms = Room.objects.all()
+    current_time = datetime.datetime.now
+    for room in rooms:
+        if room.Available(current_time):
+            available_rooms.append(room)
+    return available_rooms
